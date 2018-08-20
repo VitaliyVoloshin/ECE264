10b8f00fbb13d9a8b8029c0d7024e0cba6431bdb
astpierre <astpier@purdue.edu>
2018-08-20 11:46:10 -0400

    HW01 and HW02

---
+++ HW02/sort.c
@@ -0,0 +1,65 @@
+// ***
+// *** You MUST modify this file, only the ssort function
+// ***
+
+#include "sort.h"
+#include <stdio.h>
+#include <stdbool.h>
+
+static bool checkOrder(int * arr, int size)
+// a static function is visible only in this file
+// This function returns true if the array elements are
+// in the ascending order.
+// false, otherwise
+{
+  int ind;
+  for (ind = 0; ind < (size - 1); ind ++)
+    {
+      if (arr[ind] > arr[ind + 1])
+	{
+	  return false;
+	}
+    }
+  return true;
+}
+
+#ifdef TEST_SORT
+void ssort(int * arr, int size)
+{
+  // This function has two levels of for
+  // The first level specifies which element in the array
+  // The second level finds the smallest element among the unsorted
+  // elements
+
+  // After finding the smallest element among the unsorted elements,
+  // swap it with the element of the index from the first level
+  
+  // call checkOrder to see whether this function correctly sorts the
+  // elements
+  
+  int ind;
+  int * tmp;
+  int sub_ind;
+  int smallest_ind;
+
+  for (ind=0; ind < (size-1); ind++)
+  	{
+	  sub_ind = ind;
+	  for (smallest_ind=ind+1; smallest_ind<size; smallest_ind++)
+	  {
+	  	if (arr[smallest_ind] > arr[sub_ind])
+		{
+		  tmp = &arr[smallest_ind];
+		  arr[smallest_ind] = arr[sub_ind];
+		  arr[sub_ind] = *tmp;
+		}
+
+	  }
+	}
+
+  if (checkOrder(arr, size) == false)
+    {
+      fprintf(stderr, "checkOrder returns false\n");
+    }
+}
+#endif
