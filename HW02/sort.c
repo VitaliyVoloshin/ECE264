215bc7219a679690369104e0f62ee64675d1c60a
astpierre <astpier@purdue.edu>
2018-08-21 18:53:53 -0400

    working clean version

--- HW02/sort.c
+++ HW02/sort.c
@@ -25,50 +25,48 @@ static bool checkOrder(int * arr, int size)
 
 #ifdef TEST_SORT
 void ssort(int * arr, int size)
-{
-  // This function has two levels of for
-  // The first level specifies which element in the array
-  // The second level finds the smallest element among the unsorted
-  // elements
-
-  // After finding the smallest element among the unsorted elements,
-  // swap it with the element of the index from the first level
-  
-  // call checkOrder to see whether this function correctly sorts the
-  // elements
-  
-  int ind;
-  int sub_ind;
-  int tmp = 0;
+{  
+  int ind;		// Select sort
+  int sub_ind;	// Select sort
+  int tmp = 0;	// Swapping
 
   for (ind=0; ind < size; ++ind)
-  	{
-	  sub_ind = ind;
+  	{	
 	  for (sub_ind=ind+1; sub_ind<size; ++sub_ind)
 	  {
+	  	// Items for comparison
 	    int next = arr[sub_ind];
 		int current = arr[ind];
-
-		char current_len[100];
-		char next_len[100];
+		
+		// Making character arrays of values to fix 
+		// lexicographical issue when comparing vals.
+		// ex. of problem 1,2,10 instead of 1,10,2
+		char current_len[1000];
+		char next_len[1000];
 		sprintf(current_len,"%d",current);
-		sprintf(next_len,"%d",next); 
+		sprintf(next_len,"%d",next);
+		
+		// sizeof returns length of arrays
 		int cl = 0;
 		int nl = 0;
-		for (cl=0;current_len[cl]!='\n';++cl);
-		for (nl=0;next_len[nl]!='\n';++nl);
-		/*
+		cl = sizeof current_len;
+		nl = sizeof next_len;	
+		
+		// If lengths are equal, treat normally
 		if (cl==nl)
 		{ 
 		  if (current > next)
 		  {
-		    tmp = arr[sub_ind];
+		    // Swap locations in array
+			tmp = arr[sub_ind];
 		    arr[sub_ind] = arr[ind];
 		    arr[ind] = tmp;
 		  }
-		}*/
-		if (cl > nl)
+		}
+		// If not, catch exception and override
+		else if (cl > nl)
 		{
+			// Swap locations in array
 			tmp = arr[sub_ind];
 			arr[sub_ind] = arr[ind];
 			arr[ind] = tmp;
