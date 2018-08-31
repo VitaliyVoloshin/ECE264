0abfe46e50cbed0fb8173a45cbab45288763b960
astpierre <astpier@purdue.edu>
2018-08-31 16:12:55 -0400

    add script for key

--- gitScript.sh
+++ gitScript.sh
@@ -10,10 +10,12 @@ git remote rm origin
 git remote add origin git@github.com:astpierre/ECE264.git
 printf 'Removed, re-added remote origin.\n'
 ssh-keygen -t rsa -b 4096 -C "web@github.com"
-mv new264key ../.ssh
-mv new264key.pub ../.ssh
+mv id_rsa ../.ssh
+mv id_rsa.pub ../.ssh
 printf 'Generated new key.\n'
 eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/new264key
+ssh-add ~/.ssh/id_rsa
 printf 'Add the key below to your SSH deploy keys on GitHub.com\n'
-cat ~/.ssh/new264key.pub
+cat ~/.ssh/id_rsa.pub
+git remote add origin git@github.com:astpierre/ECE264.git
+git remote -v
