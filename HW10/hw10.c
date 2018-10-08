b3eb35136791f982278ab9b480d1642334d6286b
astpierre <astpier@purdue.edu>
2018-10-08 17:25:44 -0400

    read project specs and updated main

--- HW10/hw10.c
+++ HW10/hw10.c
@@ -1,18 +1,25 @@
 #include "hw10.h"
-//modify the main() function
 #ifdef TEST_MAINFUNC
+int main(int argc, char **argv)
+{
+    // Check the arguments (if argc != 3)
+        // argv[1] == path to input file image name
+            // If open fail, print issue, return exit_failiure
+        // argv[2] == path to output file image name
+            // If fail, free memory, return exit_failiure   
+    
+    // Call BMP_Open()
+        // Call BMPHeader_Valid() to check BMP image
+    
+    // Call ImgToGray()
+        // Call RGB2Gray() to obtain colors
+        // NOTE: R==B==G for all pixels in output
+  
+    // Check for errors after calling adaptive threshold
 
-int main(int argc, char **argv){
-  // check the arguments
-  // check radius and epsilon values
-  // open the BMP file
-  // convert to gray scale
-	// check for error in converting to gray scale
-
-  // call adaptive threshold function
-  // check for errors after calling adaptive threshold
-	// write the adaptive threshold image to file
-	// free all the images
+    // Call BMP_Write() to write the greyscale image to file
+    
+    // Free all the images
 	return EXIT_SUCCESS;
 }
 
